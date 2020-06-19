ef6b81dde428534c25ac60ff9f4ac4b1b8c450cd
ladshubham4 <42617250+ladshubham4@users.noreply.github.com>
2020-06-20 02:27:15 +0530

    Add files via upload
    Simple random quotes generator using JS and Vue.js

---
+++ quote-machine/main.js
@@ -0,0 +1,21 @@
+new Vue({
+  el: "#app",
+  data: {
+    quotes,
+    currentQuote: quotes[0],
+    animating: false,
+  },
+  methods: {
+    newQuote: function () {
+      const num = Math.floor(Math.random() * quotes.length);
+      this.currentQuote = quotes[num];
+    },
+  },
+  updated: function () {
+    this.animating = true;
+    setTimeout(() => {
+      this.animating = false;
+    }, 1000);
+    console.log("updated");
+  },
+});
